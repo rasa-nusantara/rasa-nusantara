3489df4455b95b40b1b76b87aaac53db5c754415
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-10-23 08:54:58 +0700

    fix after login

--- main/views.py
+++ main/views.py
@@ -9,6 +9,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
+import datetime
 
 def homepage(request):
     restaurants_entries = Restaurant.objects.all()
@@ -86,7 +87,7 @@ def login_user(request):
       if form.is_valid():
         user = form.get_user()
         login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
+        response = HttpResponseRedirect(reverse("main:homepage"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
 
