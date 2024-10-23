473c9b3a2d3ff27f8f0b3b66d2552b9875343d66
dzikrilmn <dzikri.ilmansyah@gmail.com>
2024-10-23 21:28:59 +0700

    add base for reservasi

---
+++ reservasi/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from main.models import Restaurant  # Assuming you have the Restaurant model in the 'main' app
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # The user making the reservation
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)  # The restaurant being reserved
+    reservation_date = models.DateField()  # The date of the reservation
+    reservation_time = models.TimeField()  # The time of the reservation
+    number_of_people = models.PositiveIntegerField()  # Number of people for the reservation
+    special_request = models.TextField(blank=True, null=True)  # Optional special requests
+
+    def __str__(self):
+        return f'{self.user.username} - {self.restaurant.nama_restoran} on {self.reservation_date} at {self.reservation_time}'
