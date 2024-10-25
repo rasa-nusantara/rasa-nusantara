889842c443e6c09c99241427f27e56a2b151d4b0
arishashaista <arishashaistaa@gmail.com>
2024-10-25 16:02:46 +0700

    update favorite

--- favorite/urls.py
+++ favorite/urls.py
@@ -4,6 +4,5 @@ from favorite.views import remove_favorite, favorite_list
 app_name = 'favorite'
 
 urlpatterns = [
-    path('list/', favorite_list, name='favorite_list'), 
-    path('remove/<uuid:restaurant_id>/', remove_favorite, name='remove_favorite'),  
-]
+    path('list/', favorite_list, name='favorite_list'),  # To list the favorite restaurants
+path('favorite/remove/<uuid:restaurant_id>/', remove_favorite, name='remove_favorite'),]
