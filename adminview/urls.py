2d7e9763a4eff59a5c414a094af0c1b953f54c6c
refalinoshahzada <ref4gha@gmail.com>
2024-10-26 00:14:31 +0700

    Implementasi view admin

--- adminview/urls.py
+++ adminview/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from adminview.views import admin_restaurant_view, edit_restaurant, delete_restaurant, add_restaurant
 
 app_name = 'adminview'
 
 urlpatterns = [
+    path('', admin_restaurant_view, name='admin_restaurant'),
+    path('edit/<int:id>/', edit_restaurant, name='edit_restaurant'),
+    path('delete/<int:id>/', delete_restaurant, name='delete_restaurant'),
+    path('add/', add_restaurant, name='add_restaurant'),
 ]
--- rasa_nusantara/urls.py
+++ rasa_nusantara/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('reviews/', include('review.urls')),
+    path('adminview/', include('adminview.urls')),
 ]
